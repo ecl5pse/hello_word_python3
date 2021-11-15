18721e3d60c093f41b7aba9bba7d7bc8e73ed1ab
ecl5pse <fantamas20@gmail.com>
2021-11-15 19:22:46 +0200

    secode day

--- hello.py
+++ hello.py
@@ -1,4 +1,49 @@
-with open('mydata.txt') as fp:
-    for line in iter(fp.readline, ''):
-        process_lin = (line)
-        print(process_lin)
+#with open('mydata.txt') as fp:
+ #   for line in iter(fp.readline, ''):
+  #      process_lin = (line)
+  #      print(process_lin)
+#print("Результат ",7,15, sep="") 
+#a = 5
+#b= "fff"
+#c = "4"
+
+#print(str(a)+str(a))
+
+"""
+num1= int(input("Введите первое число:"))
+num2= int(input("Введите второе число:"))
+
+print("+", num1+num2)
+print("/",num1/num2)
+print("*",num1*num2)
+"""
+"""
+
+data = int( input())
+
+
+if data > 5:
+    print("gg")
+else :
+    print("=")
+"""
+# from  faker import Faker
+
+# faker = Faker()
+# print(faker.text())
+
+
+# user_data = int(input("Введите число: "))
+
+# isHappy = True
+
+# if isHappy or user_data == 6:
+#     print("User is happy")
+# elif user_data == 5:
+#     print("Number is 5")
+# elif user_data == 7:
+#     print("Number is 7")
+# else:
+#     print("dd")
+
+
